7ee8ace61e9ba1d259be57e8350ab0cfc36c19d0
Manuel Holzhauser <manuel@mrh-development.de>
2014-03-24 12:17:34 +0100

    Updates Branch Info

--- ffnetlist.user.js
+++ ffnetlist.user.js
@@ -88,7 +88,7 @@ function storyParser()
     var _IGNORE_NEW_VERSION = false;
 
     var _VERSION = '4.5.6';
-    var _BRANCH = 'dev';
+    var _BRANCH = 'master';
 
     var _LOAD_INTERNAL = false;
 
